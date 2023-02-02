3415c803000114f4157b0f94415450c64c1026ba
Stiven Gomez <stivengomez@soyyo.co>
2023-02-02 13:02:02 -0500

    FIX - Ajuste de color background.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wild_movies_flutter/core/paths/app_paths.dart';
+import 'package:wild_movies_flutter/gui/app_style.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: AppStyle.backgroundColor
       ),
       routes: routesApp(),
       initialRoute: 'home',
