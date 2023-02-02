22bff337dee938c8fc86a81d5ca9af40704def32
Stiven Gomez <stivengomez@soyyo.co>
2023-02-02 15:50:28 -0500

    FIX - Ajuste de colores.

--- lib/gui/app_style.dart
+++ lib/gui/app_style.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class AppStyle {
 
 
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color primaryColor = Color(0xFFFFD233);
+  static const Color bottomNavigationBarColor = Color(0xFF0F0F0F);
+  static const Color greyColor = Color(0xFF8C8C8C);
   static const Color backgroundColor = Color(0xFF191919);
   static const Color transparent = Colors.transparent;
 
