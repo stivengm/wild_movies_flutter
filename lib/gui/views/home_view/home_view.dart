314d45ab75d108e1f4c61c8c17c521780bba3cb2
Stiven Gomez <stivengomez@soyyo.co>
2023-02-02 13:18:08 -0500

    FIX - Ajuste de HomeView.

--- lib/gui/views/home_view/home_view.dart
+++ lib/gui/views/home_view/home_view.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:wild_movies_flutter/gui/widgets/app_bar_widget.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({ Key? key }) : super( key: key );
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const AppBarWidget(),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
     return Container();
   }
+
 }
