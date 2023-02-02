063b864b2b4631c26c939340215378e71ec3fa41
Stiven Gomez <stivengomez@soyyo.co>
2023-02-02 16:04:33 -0500

    FIX - Ajuste de PageView para Home.

--- lib/gui/views/home_view/page_view.dart
+++ lib/gui/views/home_view/page_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wild_movies_flutter/gui/app_style.dart';
+import 'package:wild_movies_flutter/gui/widgets/header_text_widget.dart';
 
 class PageViewWidget extends StatefulWidget {
   final PageController pageController;
@@ -16,19 +18,68 @@ class _PageViewWidgetState extends State<PageViewWidget> {
       controller: widget.pageController,
       scrollDirection: Axis.horizontal,
       children: [
-        Container(
-          color: Colors.red,
+        Column(
+          children: [
+            _popular(),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Divider(
+                color: AppStyle.greyColor,
+              ),
+            ),
+          ],
         ),
         Container(
-          color: Colors.black,
         ),
         Container(
-          color: Colors.blue,
         ),
         Container(
-          color: Colors.yellow,
         ),
       ],
     );
   }
+
+  Widget _popular() {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const HeaderTextWidget(text: 'Popular'),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(),
+                GestureDetector(
+                  onTap: () {
+                    print("Ver más...");
+                  },
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Ver más",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6!
+                              .copyWith(
+                                  color: AppStyle.primaryColor,
+                                  fontSize: 13.0)),
+                      const Icon(
+                          size: 17.0,
+                          color: AppStyle.primaryColor,
+                          Icons.chevron_right_outlined)
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
 }
