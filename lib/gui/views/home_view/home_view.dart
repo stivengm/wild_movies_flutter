fc52d036a63240d5585836a4a1f12ea349be44f2
Stiven Gomez <stivengomez@soyyo.co>
2023-02-02 16:03:40 -0500

    FIX - Ajuste de homeScreen.

--- lib/gui/views/home_view/home_view.dart
+++ lib/gui/views/home_view/home_view.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:wild_movies_flutter/core/blocs/home_bloc/home_bloc.dart';
 import 'package:wild_movies_flutter/gui/app_style.dart';
 import 'package:wild_movies_flutter/gui/views/home_view/page_view.dart';
 import 'package:wild_movies_flutter/gui/widgets/app_bar_widget.dart';
-import 'package:wild_movies_flutter/gui/widgets/header_text_widget.dart';
 
 class HomeView extends StatefulWidget {
-  const HomeView({ Key? key }) : super( key: key );
+  const HomeView({Key? key}) : super(key: key);
 
   @override
   State<HomeView> createState() => _HomeViewState();
@@ -17,96 +18,38 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const AppBarWidget(),
-      body: PageViewWidget(pageController: pageController,),
-      // body: Column(
-      //   children: [
-      //     _popular(context),
-      //     const Padding(
-      //       padding: EdgeInsets.symmetric(horizontal: 20.0),
-      //       child: Divider(
-      //         color: AppStyle.greyColor,
-      //       ),
-      //     ),
-      //   ],
-      // ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: AppStyle.bottomNavigationBarColor,
-        elevation: 0,
-        selectedItemColor: AppStyle.primaryColor,
-        unselectedItemColor: AppStyle.greyColor,
-        onTap: (i) => getNameScreen(i),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              MdiIcons.homeOutline
-            ),
-            label: 'Home'
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: const AppBarWidget(),
+          body: PageViewWidget(
+            pageController: pageController,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              MdiIcons.heartOutline
-            ),
-            label: 'Favoritos'
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: AppStyle.bottomNavigationBarColor,
+            selectedItemColor: AppStyle.primaryColor,
+            unselectedItemColor: AppStyle.greyColor,
+            currentIndex: state.currentIndex,
+            onTap: (i) => getNameScreen(i),
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(MdiIcons.homeOutline), label: 'Inicio'),
+              BottomNavigationBarItem(
+                  icon: Icon(MdiIcons.heartOutline), label: 'Favoritos'),
+              BottomNavigationBarItem(
+                  icon: Icon(MdiIcons.restart), label: 'Reciente'),
+              BottomNavigationBarItem(
+                  icon: Icon(MdiIcons.magnify), label: 'Buscar'),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              MdiIcons.restart
-            ),
-            label: 'Reciente'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              MdiIcons.magnify
-            ),
-            label: 'Buscar'
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _popular() {
-    return Container(
-      width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const HeaderTextWidget(text: 'Popular'),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const SizedBox(),
-                GestureDetector(
-                  onTap: () {
-                    print("Ver más...");
-                  },
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Ver más", style: Theme.of(context).textTheme.headline6!.copyWith( color: AppStyle.primaryColor, fontSize: 13.0 )),
-                      const Icon(
-                        size: 17.0,
-                        color: AppStyle.primaryColor,
-                        Icons.chevron_right_outlined
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 
   getNameScreen(i) {
+    final homeBloc = BlocProvider.of<HomeBloc>(context);
     String nameScreen = "";
     switch (i) {
       case 0:
@@ -121,6 +64,9 @@ class _HomeViewState extends State<HomeView> {
       case 3:
         nameScreen = 'Buscar';
     }
-    pageController.animateToPage(i, duration: const Duration(milliseconds: 250), curve: Curves.easeOut);
+    homeBloc
+        .add(HandleChangeCurrentIndex(currentIndex: i, nameScreen: nameScreen));
+    pageController.animateToPage(i,
+        duration: const Duration(milliseconds: 250), curve: Curves.easeOut);
   }
 }
