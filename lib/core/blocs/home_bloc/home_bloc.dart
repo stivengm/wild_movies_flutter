62e400521625cd67285844bac9c7525cd6fc7c35
Stiven Gomez <stivengomez@soyyo.co>
2023-02-02 21:57:24 -0500

    FIX - Ajuste de servicios.

--- lib/core/blocs/home_bloc/home_bloc.dart
+++ lib/core/blocs/home_bloc/home_bloc.dart
@@ -1,6 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import 'package:http/http.dart' as http;
+import 'package:wild_movies_flutter/core/models/config_aplication_model.dart';
+
 part 'home_event.dart';
 part 'home_state.dart';
 
@@ -9,5 +12,31 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     on<HandleChangeCurrentIndex>((event, emit) {
       emit( state.copyWith( currentIndex: event.currentIndex, nameScreen: event.nameScreen ) );
     });
+
+    on<HandleLoading>((event, emit) {
+      emit( state.copyWith( isLoadingConfig: event.isLoadingConfig ) );
+    });
+
   }
+
+  getConfiguration() async {
+    add( const HandleLoading(isLoadingConfig: true) );
+    var uri = Uri.https('wildmovies-3f7eb-default-rtdb.firebaseio.com', 'aplication.json');
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final ConfigAplicationModel configAplicationModel = ConfigAplicationModel.fromRawJson(response.body);
+      add( const HandleLoading(isLoadingConfig: false) );
+    }
+
+  }
+
+  getPopular() async {
+    // Loading true!
+    try {
+      var url = Uri.https('');
+    } catch (e) {
+      
+    }
+  }
+
 }
