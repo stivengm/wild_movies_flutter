328737aceb20ed03e364725bd03d78c76c859d0f
Stiven Gomez <stivengomez@soyyo.co>
2023-03-02 21:56:10 -0500

    FIX - Bloc provider para search bloc.

--- lib/core/providers/bloc_provider.dart
+++ lib/core/providers/bloc_provider.dart
@@ -1,9 +1,11 @@
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wild_movies_flutter/core/blocs/home_bloc/home_bloc.dart';
+import 'package:wild_movies_flutter/core/blocs/search/search_bloc.dart';
 import 'package:wild_movies_flutter/core/blocs/see_more_bloc/see_more_bloc.dart';
 
 final List<BlocProvider> providerBloc = [
   BlocProvider<HomeBloc>(create: ( _ ) => HomeBloc()),
-  BlocProvider<SeeMoreBloc>(create: ( _ ) => SeeMoreBloc())
+  BlocProvider<SeeMoreBloc>(create: ( _ ) => SeeMoreBloc()),
+  BlocProvider<SearchBloc>(create: ( _ ) => SearchBloc())
 ];
