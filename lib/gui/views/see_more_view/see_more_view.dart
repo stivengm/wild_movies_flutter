0a466c3dde1f8f1512a14e4a411f4b9c4dacba83
Stiven Gomez <stivengomez@soyyo.co>
2023-03-02 23:39:17 -0500

    FIX - Ajuste en seeMoreView para mostrar imagen de fondo.

--- lib/gui/views/see_more_view/see_more_view.dart
+++ lib/gui/views/see_more_view/see_more_view.dart
@@ -17,45 +17,52 @@ class SeeMoreView extends StatelessWidget {
     final args = ModalRoute.of(context)!.settings.arguments as SeeMoreViewArguments;
     return Scaffold(
       appBar: AppBarWidget(nameScreen: args.titleSection, centerTitle: false),
-      body: BlocBuilder<HomeBloc, HomeState>(
-        builder: (context, state) {
-          return Container(
-              padding: const EdgeInsets.symmetric(vertical: 15.0),
-              child: Column(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: cardSwiper(context)
+      body: Stack(
+        children: [
+          const _Background(),
+          BlocBuilder<HomeBloc, HomeState>(
+            builder: (context, state) {
+              return Container(
+                  padding: const EdgeInsets.symmetric(vertical: 15.0),
+                  decoration: BoxDecoration(
+                    color: AppStyle.backgroundColor.withOpacity(.9)
                   ),
-                  BlocBuilder<SeeMoreBloc, SeeMoreState>(
-                    builder: (context, stateSeeMore) {
-                      return Container(
-                        padding: const EdgeInsets.symmetric(vertical: 40.0),
-                        child: Column(
-                          children: [
-                            Text(stateSeeMore.nameMovie ?? state.popularesMovies!.results![0].name!, style: Theme.of(context).textTheme.headline6!.copyWith( color: AppStyle.whiteColor, fontSize: 28.0 ),),
-                            const SizedBox(height: 20.0),
-                            StarsRatings(voteAverage: stateSeeMore.voteAverage ?? state.popularesMovies!.results![0].voteAverage!),
-                            const SizedBox(height: 20.0),
-                            Text("IMDb: ${stateSeeMore.voteAverage ?? state.popularesMovies!.results![0].voteAverage!}", style: Theme.of(context).textTheme.headlineSmall!.copyWith( color: AppStyle.greyColor, fontSize: 10.0 ),),
-                            const SizedBox(height: 20.0),
-                            SizedBox(
-                              width: media.width * .4,
-                              child: const PrimaryButton(text: 'Watch now', color: AppStyle.backgroundColor)
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: cardSwiper(context)
+                      ),
+                      BlocBuilder<SeeMoreBloc, SeeMoreState>(
+                        builder: (context, stateSeeMore) {
+                          return Container(
+                            padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 10.0),
+                            child: Column(
+                              children: [
+                                Text(stateSeeMore.nameMovie ?? state.popularesMovies!.results![0].name!, style: Theme.of(context).textTheme.headline6!.copyWith( color: AppStyle.whiteColor, fontSize: 28.0 ), textAlign: TextAlign.center,),
+                                const SizedBox(height: 20.0),
+                                StarsRatings(voteAverage: stateSeeMore.voteAverage ?? state.popularesMovies!.results![0].voteAverage!),
+                                const SizedBox(height: 20.0),
+                                Text("IMDb: ${stateSeeMore.voteAverage ?? state.popularesMovies!.results![0].voteAverage!}", style: Theme.of(context).textTheme.headlineSmall!.copyWith( color: AppStyle.greyColor, fontSize: 10.0 ),),
+                                const SizedBox(height: 20.0),
+                                SizedBox(
+                                  width: media.width * .4,
+                                  child: const PrimaryButton(text: 'Watch now', color: AppStyle.backgroundColor)
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                      );
-                    },
+                          );
+                        },
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            );
-        },
+                );
+            },
+          ),
+        ],
       ),
     );
   }
-
 
   Widget cardSwiper(context) {
     Size media = MediaQuery.of(context).size;
@@ -104,11 +111,39 @@ class SeeMoreView extends StatelessWidget {
     final seeMoreBloc = BlocProvider.of<SeeMoreBloc>(context);
     seeMoreBloc.add( HandleInformationMovie(
       nameMovie: homeBloc.state.popularesMovies!.results![index].name!,
-      voteAverage: homeBloc.state.popularesMovies!.results![index].voteAverage!
+      voteAverage: homeBloc.state.popularesMovies!.results![index].voteAverage!,
+      posterPath: homeBloc.state.popularesMovies!.results![index].posterPath
     ));
   }
 
 }
+
+
+class _Background extends StatelessWidget {
+  const _Background({ Key? key }) : super( key: key );
+
+  @override
+  Widget build(BuildContext context) {
+    Size media = MediaQuery.of(context).size;
+    final homeBloc = BlocProvider.of<HomeBloc>(context);
+    return BlocBuilder<SeeMoreBloc, SeeMoreState>(
+      builder: (context, state) {
+        return Container(
+          width: double.infinity,
+          height: media.height * 1,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage('https://image.tmdb.org/t/p/w500${state.posterPath ?? homeBloc.state.popularesMovies!.results![0].posterPath}'),
+              fit: BoxFit.cover,
+              alignment: Alignment.center
+            )
+          ),
+        );
+      },
+    );
+  }
+}
+
 
 class SeeMoreViewArguments {
   final String titleSection;
